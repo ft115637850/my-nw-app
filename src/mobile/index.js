37a6294afa337a554d367ac02336e1be781ecab7
ft115637850 <gentleman000000@hotmail.com>
2018-02-03 21:47:44 +0800

    update mobile

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { COLOR, ThemeProvider, Button } from 'react-native-material-ui';
+import { COLOR, ThemeProvider, Button, ListItem, Subheader } from 'react-native-material-ui';
 import {
 	StyleSheet,
-	View
+	View,
+	Text,
+	ScrollView,
+	Platform,
+	UIManager
 } from 'react-native';
 
 const uiTheme = {
@@ -21,18 +25,49 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
 	}
 });
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
 export default class App extends Component {
+	constructor() {
+		super();
+		if (Platform.OS === 'android') {
+		  UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+		}
+	}
 	render() {
 		return (
 			<ThemeProvider uiTheme={uiTheme}>
 				<View style={styles.container}>
-					<Button primary={true} text="Primary" />
-					<Button accent={true} text="Accent" />
+					<ScrollView style={{flex: 1}}>
+						<Text style={styles.welcome}>
+						  Welcome to React Native!
+						</Text>
+						<Text style={styles.instructions}>
+						  To get started, edit App.js
+						</Text>
+						<Text style={styles.instructions}>
+						  {instructions}
+						</Text>
+					</ScrollView>
 					<Button raised={true} primary={true} text="Primary" />
-					<Button disabled={true} text="Disabled" />
 				</View>
 			</ThemeProvider>
 		);
