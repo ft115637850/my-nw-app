76d09638c087e17b74370846c653f0c502d6bda8
Zou, Newton Xing <gentleman000000@hotmail.com>
2018-02-03 18:45:48 +0800

    add material ui

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -1,57 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { COLOR, ThemeProvider, Button } from 'react-native-material-ui';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	StyleSheet,
+	View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+const uiTheme = {
+	palette: {
+		primaryColor: COLOR.green500
+	},
+	toolbar: {
+		container: {
+			height: 50
+		}
+	}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 });
+
+export default class App extends Component {
+	render() {
+		return (
+			<ThemeProvider uiTheme={uiTheme}>
+				<View style={styles.container}>
+					<Button primary={true} text="Primary" />
+					<Button accent={true} text="Accent" />
+					<Button raised={true} primary={true} text="Primary" />
+					<Button disabled={true} text="Disabled" />
+				</View>
+			</ThemeProvider>
+		);
+	}
+}
