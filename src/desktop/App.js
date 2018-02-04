d6d3e156d9e8efa39ec15ecba00f55ced0a314e3
Zou, Newton Xing <115637850@qq.com>
2018-02-04 08:45:49 +0800

    add Kiosk Mode

--- src/desktop/App.js
+++ src/desktop/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import RaisedButton from 'material-ui/RaisedButton';
-
+const gui = require('nw.gui');
 const styles = {
 	container: {
 		display: 'flex',
@@ -30,6 +30,10 @@ const muiTheme = {
 };
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { isKioskMode: false };
+	}
 	render() {
 		return (
 			<MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
@@ -37,7 +41,16 @@ class App extends Component {
 					<div style={styles.welcome}>Welcome to React Native in Node Webkit!</div>
 					<div style={styles.instructions}>To get started, edit App.js</div>
 					<p style={styles.instructions}>Double tap R on your keyboard to reload, Shake or press menu button for dev menu</p>
-					<RaisedButton label={'Primary'} primary={true} onClick={() => {}} />
+					<RaisedButton label={this.state.isKioskMode ? 'Quit' : 'Kiosk'} primary={true} onClick={() => {
+						if (this.state.isKioskMode) {
+							gui.Window.get().leaveKioskMode();
+							this.setState({ isKioskMode: false });
+						} else {
+							gui.Window.get().enterKioskMode();
+							this.setState({ isKioskMode: true });
+						}
+					}}
+					/>
 				</div>
 			</MuiThemeProvider>
 		);
